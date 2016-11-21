ebe43eebc4d198ab1451431f76ccf9dd7cf82681
Steve <urgent.programmer@gmail.com>
2016-11-21 15:57:48 -0500

    Tokbox Spike -- button event handler
    Added listener for connect button
    
    JS include on dashboard for call2text opentok implementation

---
+++ assets/global/scripts/call2search.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+    $("div.portlet-body").on("click","button.connect", function(){
+        console.log('Desktop App Connect');
+    }); //button.connect click
+}); //doc ready
