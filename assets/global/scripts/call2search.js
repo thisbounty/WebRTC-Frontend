57c590a3545eb98e522a1cc862aff924d6d5c1e5
Steve <urgent.programmer@gmail.com>
2016-11-21 16:10:04 -0500

    Tokbox Spike -- Opentok session call and definition
    Created function definition for connecting to opentok session
    
    Added call, used callback to ensure session exists without blocking

--- assets/global/scripts/call2search.js
+++ assets/global/scripts/call2search.js
@@ -1,5 +1,13 @@
 jQuery(document).ready(function(){
-    $("div.portlet-body").on("click","button.connect", function(){
-        console.log('Desktop App Connect');
+    $("div.portlet-body").on("click","button.connect", function() {
+        if (OT.checkSystemRequirements() == 1) {
+            call2text_opentok_connect(config.tokbox_api, $(this).attr('data-session'), $(this).attr('data-token'), function(session) {
+                console.log('Desktop App Connect');
+            }); //call2text_opentok_connect
+        }
     }); //button.connect click
 }); //doc ready
+
+function call2text_opentok_connect($api, $session_id, $token, cb) {
+    cb(false);
+}
