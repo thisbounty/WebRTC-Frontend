7d2c67f98542633e863c6aced58dc675cf89298c
Steve <urgent.programmer@gmail.com>
2016-11-22 14:13:22 -0500

    Tokbox Spike -- Split files | issue #2
    Split files for opentok connect, one for callers one for searchers
    
    Allows easier collab

---
+++ assets/global/scripts/call2search.searchers.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function(){
+    $("div.portlet-body").on("click","button.connect", function() {
+        if (OT.checkSystemRequirements() == 1) {
+            call2text_opentok_connect(config.tokbox_api, $(this).attr('data-session'), $(this).attr('data-token'), function(session) {
+                session.on("streamCreated", function (event) {
+                    var options = {subscribeToAudio:true, subscribeToVideo:false};
+                    session.subscribe(event.stream, replacementElementId, options);
+                }); //on streamCreated
+            }); //call2text_opentok_connect
+        } //if checkSystemRequirements
+    }); //button.connect click
+}); //doc ready
+
+function call2text_opentok_connect($api, $session_id, $token, cb) {
+    var session = OT.initSession($api, $session_id);
+    session.connect($token, function(error) {
+      if (error) {
+        console.log("Error connecting: ", error.code, error.message);
+      } else {
+          cb(session);
+      }
+    });
+}
